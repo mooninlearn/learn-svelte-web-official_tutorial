8afdd411b4cd666bd49acea35644840180cbc8ca
undefined <mooninlearn@gmail.com>
2023-06-22 14:18:54 +0900

    Special elements  svelteself

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,37 @@
 <script>
-	import Map from './Map.svelte';
-	import MapMarker from './MapMarker.svelte';
+	import Folder from './Folder.svelte';
+
+	let root = [
+		{
+			name: 'Important work stuff',
+			files: [
+				{ name: 'quarterly-results.xlsx' }
+			]
+		},
+		{
+			name: 'Animal GIFs',
+			files: [
+				{
+					name: 'Dogs',
+					files: [
+						{ name: 'treadmill.gif' },
+						{ name: 'rope-jumping.gif' }
+					]
+				},
+				{
+					name: 'Goats',
+					files: [
+						{ name: 'parkour.gif' },
+						{ name: 'rampage.gif' }
+					]
+				},
+				{ name: 'cat-roomba.gif' },
+				{ name: 'duck-shuffle.gif' },
+				{ name: 'monkey-on-a-pig.gif' }
+			]
+		},
+		{ name: 'TODO.md' }
+	];
 </script>
 
-<Map lat={35} lon={-84} zoom={3.5}>
-	<MapMarker lat={37.8225} lon={-122.0024} label="Svelte Body Shaping"/>
-	<MapMarker lat={33.8981} lon={-118.4169} label="Svelte Barbershop & Essentials"/>
-	<MapMarker lat={29.7230} lon={-95.4189} label="Svelte Waxing Studio"/>
-	<MapMarker lat={28.3378} lon={-81.3966} label="Svelte 30 Nutritional Consultants"/>
-	<MapMarker lat={40.6483} lon={-74.0237} label="Svelte Brands LLC"/>
-	<MapMarker lat={40.6986} lon={-74.4100} label="Svelte Medical Systems"/>
-</Map>
+<Folder name="Home" files={root} expanded/>
